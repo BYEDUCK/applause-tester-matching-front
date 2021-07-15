01b957d77373cf8f5c1aef100cdc55474cad44cc
byeduck <mateuszbajdak@gmail.com>
2021-07-15 22:11:51 +0200

    chore: clean up

--- src/app/main/query/query.component.ts
+++ src/app/main/query/query.component.ts
@@ -1,8 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
 import {Device} from '../../model/device-model';
-import {first, tap} from 'rxjs/operators';
-import {toQueryStringCountries, toQueryStringDevices} from './query-utils';
+import {first} from 'rxjs/operators';
 import {HttpService} from '../http.service';
 import {RankedTester} from '../../model/ranked-tester-model';
 
@@ -15,10 +14,7 @@ export class QueryComponent implements OnInit {
 
   devices$: BehaviorSubject<Device[]> = new BehaviorSubject<Device[]>([]);
   countries$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
-  query$: Observable<string>;
   devicesAndCountries$: Observable<[Device[], string[]]>;
-  readonly devicesQuery = toQueryStringDevices;
-  readonly countriesQuery = toQueryStringCountries;
 
   @Input()
   set devices(devices: Device[]) {
@@ -36,9 +32,7 @@ export class QueryComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.devicesAndCountries$ = combineLatest([this.devices$, this.countries$]).pipe(
-      tap(console.log)
-    );
+    this.devicesAndCountries$ = combineLatest([this.devices$, this.countries$]);
   }
 
   submitQuery(): void {
